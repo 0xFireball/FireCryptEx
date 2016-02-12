205db558ba9e36e0d920af5abdcf1cb0beaecd1c
0xFireball <0xFireball@outlook.com>
2016-02-11 22:25:01 -0800

    fixes for CI

---
+++ FireCrypt/Aluminum.PluginCore2/PluginInterface.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PluginInterface
+{
+	public interface IPlugin
+	{
+		IPluginHost Host {get;set;}
+		
+		string Name {get;}
+		string Description {get;}
+		string Author {get;}
+		string Version {get;}
+		
+		object HostObject {get;set;}
+		
+		void InvokePlugin();
+		
+		void Initialize();
+		void Dispose();
+	
+	}
+	
+	public interface IPluginHost
+	{
+		//void Feedback(string Feedback, IPlugin Plugin);	
+	}
+}
